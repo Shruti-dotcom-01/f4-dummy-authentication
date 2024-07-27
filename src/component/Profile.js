0a265a53429b5d31deab2a221684395932bc31b4
Shruti-Vaidya <147685533+Shruti-dotcom-01@users.noreply.github.com>
2024-07-27 20:56:40 +0530

    Add files via upload

---
+++ src/component/Profile.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import { fetchprofileReq, fetchprofileSucc, fetchProfileFail } from '../redux/ProfileActionCreatoe';
+import { useNavigate } from 'react-router-dom';
+
+const Profile = () => {
+    let x = useSelector((store) => console.log("store", store));
+    let profileData = useSelector((store) => store.profileData.data)
+    let loading = useSelector((store) => store.profileData.loading)
+
+    let dispatch = useDispatch();
+    let navigate = useNavigate();
+
+    let id = useSelector((store) => {
+        if (store.fetchPostData.data) {
+            return store.fetchPostData.data.id;
+        }
+    })
+    useEffect(() => {
+        if (id) {
+            dispatch(fetchprofileReq());
+            axios.get(`https://dummyjson.com/users/${id}`)
+                .then((res) => {
+                    dispatch(fetchprofileSucc(res.data));
+                }).catch((err) => {
+                    console.log(err);
+                    dispatch(fetchProfileFail(err.message))
+                })
+        }
+        else {
+            console.log("you are not login");
+            // setTimeout(() => { navigate('/'); }, 2000)
+            navigate('/');
+        }
+    }, [id])
+
+    return (
+        <div>
+            {
+                !loading ? (
+                    <div className='profile-cont'>
+                        {
+                            profileData && (
+                                <div className='profile'>
+                                    <h1 >Profile</h1>
+                                    <img src={profileData.image} alt="userimage" />
+                                    <div><strong>Full Name: </strong>{profileData.firstName + " " + profileData.lastName}</div>
+                                    <div><strong>Age: </strong>{profileData.age}</div>
+                                    <div><strong>gender: </strong>{profileData.gender}</div>
+                                    <div><strong>Email: </strong>{profileData.email}</div>
+                                    <div><strong>Phone: </strong>{profileData.phone}</div><br></br>
+                                    <div><strong>Company Name: </strong>{profileData.company.name}</div>
+                                    <div><strong>Post: </strong>{profileData.company.title}</div>
+                                    <div><strong>Department: </strong>{profileData.company.department}</div>
+                                    <div><strong>University: </strong>{profileData.university}</div>
+                                </div>
+                            )
+                        }
+                    </div>
+                ) : ("")
+            }
+        </div>
+    )
+}
+{/* <div className='parent'><div className='loader'></div></div> */ }
+export default Profile
